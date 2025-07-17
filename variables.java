772cb5f8111945595405460a931dd445aefd8653
Mumtaz Lone <mumtazlone1315@gmail.com>
2025-07-17 16:41:45 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum = a+b;
+        int diff = a-b;
+        int mul = a*b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+
+    }
+}
